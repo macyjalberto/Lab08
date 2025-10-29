27477de75dfa9b7b9b9149580a300f6c7b985c1d
“macyjalberto” <macyjalberto@gmail.com>
2025-10-28 21:34:00 -0600

    Implemented requested methods as well as their tests

--- app/src/main/java/com/example/lab08/CustomList.java
+++ app/src/main/java/com/example/lab08/CustomList.java
@@ -13,12 +13,62 @@ public class CustomList {
     public CustomList() {
         this.cities = new ArrayList<>();
     }
-
+    
+    /**
+     * adds City to list if City does not exist
+     * @param city: given City to add
+     * @throws IllegalArgumentException error if list already contains given City
+     */
     public void addCity(City city) {
         cities.add(city);
     }
     
+    /**
+     * gets City from list
+     * @param position: asks what position the city user wants from
+     */
+    public void getCity(int position) {
+        cities.get(position);
+    }
+    
+    /**
+     * gets size of list
+     *
+     * @return size of list
+     */
+    public int getSize() {
+        return cities.size();
+    }
+    
+    /**
+     * returns Boolean of whether given City already exist in list
+     * @param city: given City of what to check in the list
+     * @return returns Boolean
+     */
     public boolean hasCity(City city) {
         return cities.contains(city);
+    }
+    
+    /**
+     * deletes a City from the list
+     * @param city: given City of what to remove from list
+     * @throws IllegalArgumentException error if list already contains given City
+     */
+    public void deleteCity(City city) {
+        // deletes city if in list
+        if (cities.contains(city)) {
+            cities.remove(city);
+        }
+        else { // throws an Exception if the given City is not in the list
+            throw new IllegalArgumentException("This city does not exist! Please try again.");
+        }
+    }
+    
+    /**
+     * counts how many Cities are in the list
+     * @return returns the number of items in the list
+     */
+    public int countCities() {
+        return cities.size();
     }
 }
--- app/src/test/java/com/example/lab08/CustomListTest.java
+++ app/src/test/java/com/example/lab08/CustomListTest.java
@@ -1,7 +1,17 @@
 package com.example.lab08;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * class to test the methods for list of Cities
+ * @author macy [and lab TAs]
+ */
 public class CustomListTest {
+    
+    /**
+     * tests whether or not the list has the given City
+     * test both cases?
+     */
     @Test
     public void testHasCity() {
         CustomList list = new CustomList();
@@ -11,4 +21,57 @@ public class CustomListTest {
         assertTrue(list.hasCity(calgary));
     }
     
+    /**
+     * tests whether the given City was deleted
+     */
+    @Test
+    void testDeleteCity() {
+        CustomList list = new CustomList();
+        City edmonton = new City("Edmonton", "AB");
+        list.addCity(edmonton);
+        list.getCity(0);
+        assertEquals(1, list.getSize()); // counts how many cities in list
+        list.deleteCity(edmonton);
+        assertEquals(0, list.getSize());
+    }
+
+    /**
+     * tests whether deleteCity() method's Exception is thrown
+     */
+    @Test
+    void testDeleteCityException() {
+        CustomList list = new CustomList();
+        City edmonton = new City("Edmonton", "AB");
+        list.addCity(edmonton);
+        list.getCity(0);
+        assertEquals(1, list.getSize()); // counts how many cities in list
+        City city = new City("Regina", "Saskatchewan");
+        assertThrows(IllegalArgumentException.class, () -> {
+            list.deleteCity(city); // checks if city that does not exist in list throws an exception
+        });
+        assertEquals(1, list.getSize()); // should still be one because nothing was deleted
+    }
+
+    /**
+     * tests whether the given amount of Cities in the list is correct
+     */
+    @Test
+    void testCountCities() {
+        CustomList list = new CustomList();
+        assertEquals(0, list.countCities());
+        City yellowknife = new City("Yellowknife", "Northwest Territories");
+        list.addCity(yellowknife);
+        assertEquals(1, list.countCities());
+        City charlottetown = new City("Charlottetown", "Prince Edward Island");
+        list.addCity(charlottetown);
+        assertEquals(2, list.countCities());
+        City regina = new City("Regina", "Saskatchewan");
+        list.addCity(regina);
+        assertEquals(3, list.countCities());
+        // deletes City to see if counts right
+        // (since we've already tested whether deleteCity works right)
+        list.deleteCity(regina);
+        assertEquals(2, list.countCities());
+    }
+
 }
